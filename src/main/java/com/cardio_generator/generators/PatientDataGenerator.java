4d9c262a4e7b6e46e614232a5469d622386bb9b9
BechirMelliti <166705207+BechirMelliti@users.noreply.github.com>
2024-06-08 13:00:44 +0200

    Update PatientDataGenerator.java

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -1,7 +1,16 @@
 package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
+/**
+* declares method to generate a data
+*/    
+
 
 public interface PatientDataGenerator {
+    /**
+    * it generates data for the patient that gave her using the strategy that you chose
+    * @param patientId patient identifier (integer)
+    * @param outputStrategy strategy of the output data
+    */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
