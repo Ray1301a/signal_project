2a8fb42a71bc0f2f75e81c67bc39ccfe710fea66
BechirMelliti <166705207+BechirMelliti@users.noreply.github.com>
2024-06-09 21:05:04 +0200

    Update WebSocketOutputStrategy.java

--- src/main/java/com/cardio_generator/outputs/WebSocketOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/WebSocketOutputStrategy.java
@@ -3,13 +3,23 @@ package com.cardio_generator.outputs;
 import org.java_websocket.WebSocket;
 import org.java_websocket.server.WebSocketServer;
 
+import com.alerts.AlertManager;
+import com.data_management.DataStorage;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class WebSocketOutputStrategy implements OutputStrategy {
 
     private WebSocketServer server;
+    private WebSocketClient
 
-    public WebSocketOutputStrategy(int port) {
+    public WebSocketOutputStrategy(int port) throws URISyntaxException {
+        InetSocketAddress address = new InetSocketAddress(port);
         server = new SimpleWebSocketServer(new InetSocketAddress(port));
         System.out.println("WebSocket server created on port: " + port + ", listening for connections...");
         server.start();
@@ -23,6 +33,22 @@ public class WebSocketOutputStrategy implements OutputStrategy {
             conn.send(message);
         }
     }
+
+    public void sendDataFromFile(String filePath) {
+    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        String line;
+        while ((line = br.readLine()) != null) {
+            // Send the line to each connected WebSocket client
+            server.getConnections().forEach(conn -> {
+                conn.send(line);
+                System.out.println("Sent data: " + line);
+            });
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
 
     private static class SimpleWebSocketServer extends WebSocketServer {
 
